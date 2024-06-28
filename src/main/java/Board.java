c81184817a83e3a3a447ee6cf97e587b78538e04
Tipson1 <bajaliev1997@gmail.com>
2024-06-28 09:56:37 +0200

    Klasse Board wurde ein 2d Array erstellt, ein Konstruktor und die reset Methode hinzugefügt

--- src/main/java/Board.java
+++ src/main/java/Board.java
@@ -1,2 +1,16 @@
 public class Board {
+    private char[][] board;
+
+    public Board() {
+        board = new char[3][3];
+        reset();
+    }
+
+    public void reset() {
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                board[i][j] = '-';
+            }
+        }
+    }
 }
