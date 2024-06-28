af7a0c39c47c9aa995f472f8d29ee4f05ceca08f
Tipson1 <bajaliev1997@gmail.com>
2024-06-28 10:20:47 +0200

    boolean checkWin hinzugefügt

--- src/main/java/Board.java
+++ src/main/java/Board.java
@@ -30,5 +30,21 @@ public class Board {
         board[row][col] = symbol;
         return true;
     }
+    public boolean checkWin(char symbol) {
+        for (int i = 0; i < 3; i++) {
+            if (board[i][0] == symbol && board[i][1] == symbol && board[i][2] == symbol) {
+                return true;
+            }
+            if (board[0][i] == symbol && board[1][i] == symbol && board[2][i] == symbol) {
+                return true;
+            }
+        }
+        if (board[0][0] == symbol && board[1][1] == symbol && board[2][2] == symbol) {
+            return true;
+        }
+        if (board[0][2] == symbol && board[1][1] == symbol && board[2][0] == symbol) {
+            return true;
+        }
+        return false;
+    }
 }
-
