511151b3945f0e1f806362986c88e8aee88c5f95
Tipson1 <bajaliev1997@gmail.com>
2024-06-28 11:31:10 +0200

    Ausführmethode wurde hinzugefügt

--- src/main/java/TicTacToe.java
+++ src/main/java/TicTacToe.java
@@ -49,4 +49,9 @@ public class TicTacToe {
             }
         }
     }
+
+    public static void main(String[] args) {
+        TicTacToe game = new TicTacToe();
+        game.start();
+    }
 }
